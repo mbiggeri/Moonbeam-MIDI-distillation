709c1278a984bad3e00eb1357cdafbb4524c0620
guozixunnicolas <nicolas.guozixun@gmail.com>
2024-07-03 17:57:11 +0100

    add output attention

--- src/llama_recipes/transformers_minimal/build/lib/transformers/modeling_utils.py
+++ src/llama_recipes/transformers_minimal/build/lib/transformers/modeling_utils.py
@@ -1448,7 +1448,7 @@ class PreTrainedModel(nn.Module, ModuleUtilsMixin, GenerationMixin, PushToHubMix
                     ' We recommend to just use `attn_implementation="flash_attention_2"` when loading the model.'
                 )
 
-            if config._attn_implementation not in ["eager", "sdpa", "flash_attention_2"]:
+            if config._attn_implementation not in ["eager", "sdpa", "flash_attention_2", "output"]:
                 message = f'Specified `attn_implementation="{config._attn_implementation}"` is not supported. The only possible arguments are `attn_implementation="eager"` (manual attention implementation)'
                 if cls._supports_flash_attn_2:
                     message += ', `"attn_implementation=flash_attention_2"` (implementation using flash attention 2)'
