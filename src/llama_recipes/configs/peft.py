e778250d61fb21b6bd47b801fd85ae693a2eae2b
guozixunnicolas <nicolas.guozixun@gmail.com>
2024-12-25 20:31:21 +0000

    add emo con gen model config

--- src/llama_recipes/configs/peft.py
+++ src/llama_recipes/configs/peft.py
@@ -8,7 +8,8 @@ from typing import List
 class lora_config:
      r: int=8
      lora_alpha: int=32
-     target_modules: List[str] = field(default_factory=lambda: ["q_proj", "v_proj"])
+     target_modules: List[str] = field(default_factory=lambda: ["q_proj", "v_proj", "k_proj", "o_proj"])
+     modules_to_save: List[str] = field(default_factory=lambda: ["supplementary_embedding_emotion4Q"])
      bias= "none"
      task_type: str= "CAUSAL_LM"
      lora_dropout: float=0.05
