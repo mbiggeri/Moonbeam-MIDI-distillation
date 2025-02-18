a20c4dbacc92a0753cfc6185c5d5b59e5819d87f
guozixunnicolas <nicolas.guozixun@gmail.com>
2025-02-18 09:30:32 +0000

    wandb changes dir name, lora adds trainable layer

--- src/llama_recipes/configs/peft.py
+++ src/llama_recipes/configs/peft.py
@@ -9,7 +9,7 @@ class lora_config:
      r: int=8
      lora_alpha: int=32
      target_modules: List[str] = field(default_factory=lambda: ["q_proj", "v_proj", "k_proj", "o_proj"])
-     modules_to_save: List[str] = field(default_factory=lambda: ["supplementary_embedding_emotion4Q"])
+     modules_to_save: List[str] = field(default_factory=lambda: ["supplementary_embedding_metadata"])
      bias= "none"
      task_type: str= "CAUSAL_LM"
      lora_dropout: float=0.05
