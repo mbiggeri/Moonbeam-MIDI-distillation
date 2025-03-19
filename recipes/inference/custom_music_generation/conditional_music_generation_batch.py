04f89f02ca5ab1a673860bab3215ca283794e304
guozixunnicolas <nicolas.guozixun@gmail.com>
2025-03-19 18:36:02 +0000

    Add a generation script that sequentially generate midi based on a CSV file.

--- recipes/inference/custom_music_generation/conditional_music_generation_batch.py
+++ recipes/inference/custom_music_generation/conditional_music_generation_batch.py
@@ -13,34 +13,48 @@ from generation import MusicLlama
 import random
 import ast
 import json
-def find_chord_string(filename, csv_file):
-    df = pd.read_csv(csv_file)
-    chord_str = df[df['id'] == filename.split("_")[-1][:-4]]['chord_progressions'].values[0]
-    return chord_str
-def metadata_id2label(metadata_id, additional_token_dict_inv):
-    label = [additional_token_dict_inv[x].replace("/",".") for x in metadata_id]
-    return "-".join(label)
-def count_notes_in_range(tokens, min_val, max_val):
-    correct = sum(1 for token in tokens if min_val <= token <= max_val)
-    return correct
+import miditoolkit
+from mido import MidiFile, MidiTrack, Message, bpm2tempo, MetaMessage
+from music21 import chord
+from music21 import harmony
+# Function to convert chord symbols to MIDI notes using music21
+def chord_to_midi(chord_symbol):
+    # Create a ChordSymbol object
+    chord_obj = harmony.ChordSymbol(chord_symbol)
+    # Get the pitches of the chord
+    pitches = chord_obj.pitches
+    # Return the pitch names
+    out = [p.midi  for p in pitches]
+    return out
 
-def find_mapping_vel(vel_token, additional_token_dict_inv):
-    return int(additional_token_dict_inv[vel_token].split("_")[-1])
+def create_midi_from_chords(chord_list, is_incomplete_measure, ticks_per_bar, bpm=220):
+    mid = MidiFile()
+    track = MidiTrack()
+    mid.tracks.append(track)
 
-def find_mapping_pitch(pitch_token, additional_token_dict_inv):
-    range_str = "_".join(additional_token_dict_inv[pitch_token].split("_")[2:])
-    RANGE_MAP = {
-        "very_low": (12, 23),       # C-2 to B0
-        "low": (24, 35),           # C1 to B1
-        "mid_low": (36, 47),       # C2 to B2
-        "mid": (48, 59),           # C3 to B3
-        "mid_high": (60, 71),      # C4 to B4
-        "high": (72, 83),          # C5 to B5
-        "very_high": (84, 107)     # C6 to G8
-    }
+    # Convert BPM to MIDI tempo (microseconds per quarter note)
+    tempo = bpm2tempo(bpm)
 
-    return RANGE_MAP[range_str]
+    # Start time accounting for incomplete measure
+    start_time = is_incomplete_measure * ticks_per_bar
 
+    # Set the tempo using a MetaMessage
+    track.append(MetaMessage('set_tempo', tempo=tempo, time=0))
+
+    # Set instrument (optional)
+    track.append(Message('program_change', program=0, time=start_time))
+
+    for chord_symbol in chord_list:
+        notes = chord_to_midi(chord_symbol)  # Convert chord to MIDI note numbers
+        for note in notes:
+            track.append(Message('note_on', note=note, velocity=64, time=0))
+
+        ts = mid.ticks_per_beat // 2  # Set duration to an eighth note
+        for note in notes:
+            track.append(Message('note_off', note=note, velocity=64, time=ts))
+            ts = 0  # Ensure only first note-off has delay
+
+    return mid
 
 def main(
     ckpt_dir: str,
@@ -51,12 +65,12 @@ def main(
     top_p: float = 0.9,
     max_seq_len: int = 512,
     max_batch_size: int = 4,
-    num_test_data: int = 50,
     if_add_chords_in_transformer: bool = True,
     if_add_metadata_in_transformer: bool = False,
     max_gen_len: Optional[int] = None,
     finetuned_PEFT_weight_path: Optional[str] = None,
     additional_token_dict_path: Optional[str] = None,
+    chord_dict_path: Optional[str] = None
 ):
     """
     Examples to run with the models finetuned for chat. Prompts correspond of chat
@@ -81,123 +95,114 @@ def main(
     with open(additional_token_dict_path, "r") as f:
         additional_token_dict = json.load(f)
 
+    save_folder = os.path.join(finetuned_PEFT_weight_path, os.path.basename(ckpt_dir), f"temperature_{temperature}_top_p_{top_p}")
+    os.makedirs(save_folder, exist_ok=True)
+
     additional_token_dict_inv = {v: k for k, v in additional_token_dict.items()}
     generator = MusicLlama.build_commu_con_gen(
         ckpt_dir=ckpt_dir,
-        model_config_path = model_config_path,  #TODO: check where to add conditions
+        model_config_path = model_config_path,
         tokenizer_path=tokenizer_path,
         max_seq_len=max_seq_len,
         max_batch_size=max_batch_size,
         finetuned_PEFT_weight_path = finetuned_PEFT_weight_path,
         additional_token_dict = additional_token_dict
     ) 
-
-    df = pd.read_csv(csv_file)
-
-    # 2. Find the row where 'split' is 'test' and get the corresponding .npy file path
-    split = "val"
-    test_filenames = df[df['split'] == split]['file_base_name'].tolist() #change from test to train, since there's really no test data in LLM
-    test_metadata_ids = df[df['split'] == split]['label'].tolist()
-    test_metadata_ids = [list(ast.literal_eval(l)) for l in test_metadata_ids]
-    test_filenames_chord = df[df['split'] == split]['chord_file_base_name'].tolist()
-    
-    #temporary fix: find chord symbols from original csv file
-    test_filenames_chord_str = [find_chord_string(x, "/data/scratch/acw753/datasets/commu/commu_meta.csv") for x in test_filenames]
-
-    test_files = list(zip(test_filenames, test_metadata_ids, test_filenames_chord, test_filenames_chord_str))
-    test_files_sampled = random.sample(test_files, num_test_data)
-    prompts = []
-    metadata_condition_decoder = []
-    chord_condition_decoder = []
-    bpm_condition = []
-    time_signature_condition = []
-    num_measures_condition = []
     if_add_chords_in_decoder = generator.model.if_add_chord_in_decoder
     if_add_metadata_in_decoder = generator.model.if_add_metadata_in_decoder
     print(f"if_add_chords_in_transformer: {if_add_chords_in_transformer}, if_add_metadata_in_transformer: {if_add_metadata_in_transformer}")
     print(f"if_add_chords_in_decoder: {if_add_chords_in_decoder}, if_add_metadata_in_decoder: {if_add_metadata_in_decoder}")
-    for filename, metadata_id, chord_file_name, chord_str in test_files_sampled:
-        print(f"Loading filename: {filename}, chord_file_name: {chord_file_name}")
-        raw_tokens = np.load(os.path.join(os.path.dirname(csv_file), "processed",filename))
-        raw_tokens_chord = np.load(os.path.join(os.path.dirname(csv_file), "processed",chord_file_name))
-        metadata_condition_decoder.append(metadata_id)
-        bpm_condition.append(int(additional_token_dict_inv[metadata_id[3]].split("_")[-1]))
-        time_signature_condition.append(additional_token_dict_inv[metadata_id[-3]].split("_")[-1])
-        num_measures_condition.append(int(additional_token_dict_inv[metadata_id[2]].split("_")[-1]))
-        chord_condition_decoder.append(ast.literal_eval(chord_str)[0]) #TODO: think of what to add
-        metadata_id = [[x for _ in range(6)] for x in metadata_id] #TODO: think of a better way to do this
-        test_data_with_sos = generator.tokenizer.encode_series_con_gen_commu(raw_tokens, raw_tokens_chord, metadata_tokens = metadata_id, if_only_keep_condition_tokens = True, if_add_chords_in_transformer = if_add_chords_in_transformer, if_add_metadata_in_transformer = if_add_metadata_in_transformer)
-        prompts.append(test_data_with_sos)
+    df = pd.read_csv(csv_file)
+    for _, row in df.iterrows():
+        if row["split_data"] == "train":
+            continue
+        #set save path
+        chord_save_path  = os.path.join(save_folder, f"{row['id']}_chords.mid")
+        midi_save_path = os.path.join(save_folder, f"{row['id']}.mid")
+        metadata_save_path = os.path.join(save_folder, f"{row['id']}_metadata.json")
+
+        #get necessary conditions
+        bpm_condition = [row["bpm"]]
+        time_signature_condition = [row["time_signature"]]
+        num_measures_condition = [row["num_measures"]]
+        chord_condition_str = ast.literal_eval(row["chord_progressions"])[0]
+
+        metadata_id = [
+            additional_token_dict["audio_key_" + row["audio_key"]],
+            additional_token_dict["pitch_range_" + row["pitch_range"]],
+            additional_token_dict["num_measures_" + str(row["num_measures"])],
+            additional_token_dict["bpm_" + str(row["bpm"])],
+            additional_token_dict["genre_" + row["genre"]],
+            additional_token_dict["track_role_" + row["track_role"]],
+            additional_token_dict["inst_" + row["inst"].split("-")[0]],
+            additional_token_dict["sample_rhythm_" + row["sample_rhythm"]],
+            additional_token_dict["time_signature_" + row["time_signature"]],
+            additional_token_dict["min_velocity_" + str(row["min_velocity"])],
+            additional_token_dict["max_velocity_" + str(row["max_velocity"])]
+        ]
+
+        #create and save chord midi
+        if row["num_measures"]%4==0:
+            is_incomplete_measure = False
+        else:
+            is_incomplete_measure = True
+        numerator = int(row["time_signature"].split("/")[0])
+        denominator = int(row["time_signature"].split("/")[1])
+        ticks_per_beat = 480
+        normalized_bpm = 120
+        beats_per_bar = numerator / denominator * 4
+        ticks_per_bar = int(ticks_per_beat * beats_per_bar)
+
+        chord_midi = create_midi_from_chords(chord_condition_str, is_incomplete_measure, ticks_per_bar, normalized_bpm)
+        chord_midi.save(chord_save_path)
+
+        raw_tokens_chord = generator.tokenizer.midi_to_compound(chord_save_path, calibate_to_default_tempo = True)
+        prompts = [generator.tokenizer.encode_series_con_gen_commu(None, raw_tokens_chord, metadata_tokens = [[x for _ in range(6)] for x in metadata_id], if_only_keep_condition_tokens = True, if_add_chords_in_transformer = if_add_chords_in_transformer, if_add_metadata_in_transformer = if_add_metadata_in_transformer)]
+        metadata_condition_decoder = [metadata_id] if if_add_metadata_in_decoder else None
+        chord_condition_decoder = [chord_condition_str] if if_add_chords_in_decoder else None
     
-    if not if_add_metadata_in_decoder: 
-        metadata_condition_decoder = None
-    if not if_add_chords_in_decoder:
-        chord_condition_decoder = None
+        condition_token_lengths = [len(x) for x in prompts]
 
-    condition_token_lengths = [len(x) for x in prompts]
-    if if_add_chords_in_transformer:
-        chord_token_indices = [[x.index(generator.tokenizer.soc_token_compound), x.index(generator.tokenizer.eoc_token_compound)] for x in prompts]
-    else:
-        chord_token_indices = None
-    
-    results = generator.music_completion(
-        prompts, #this controls whether condition exists in the transformer
-        bpm_condition = bpm_condition,
-        time_signature_condition = time_signature_condition,
-        num_measures_condition = num_measures_condition,
-        metadata_condition = metadata_condition_decoder, #this controls whether condition exists in the decoder
-        chord_condition = chord_condition_decoder, 
-        max_gen_len=max_gen_len,
-        temperature=temperature,
-        top_p=top_p,
-        condition_token_lengths = condition_token_lengths, #remove sos token and emotion token
-        chord_token_indices = chord_token_indices,
-        chord_dict_path = "/data/scratch/acw753/finetune/commu_con_gen/chord_dictionary.json" #temporary fix: add chord dict path
-    )
-    
-    save_folder = os.path.join(finetuned_PEFT_weight_path, os.path.basename(ckpt_dir), f"temperature_{temperature}_top_p_{top_p}")
-    os.makedirs(save_folder, exist_ok=True)
 
-    total_correct_vel = 0 
-    total_vel = 0 
-    total_correct_pitch = 0 
-    total_pitch = 0 
-    difference_chord_music_ending_time = []
-    for i, (dialog, result, metadata_id) in enumerate(zip(prompts, results, [x[1] for x in test_files_sampled])):
+        if if_add_chords_in_transformer:
+            chord_token_indices = [[x.index(generator.tokenizer.soc_token_compound), x.index(generator.tokenizer.eoc_token_compound)] for x in prompts]
+        else:
+            chord_token_indices = None
 
-        epoch_step = re.search(r'(\d+-\d+)\.pt$', ckpt_dir).group(1)
-        for j, (sub_mid, chord_mid, tokens, chord_tokens) in enumerate(zip(result['generation']['content'], result['generation']['chord'], result['generation']['tokens'], result['generation']['chord_tokens'])):
-            save_path = f'{save_folder}/{epoch_step}_{str(i)}_{str(j)}_meta_{metadata_id2label(metadata_id, additional_token_dict_inv)}.mid'
-            try:
-                sub_mid.save(save_path)
-                chord_mid.save(save_path.replace(".mid", "_chord.mid"))
-                print(f"midis saved at {save_path}")
-                print(f"tokens: {tokens}")
+        results = generator.music_completion(
+            prompts,
+            bpm_condition = bpm_condition,
+            time_signature_condition = time_signature_condition,
+            num_measures_condition = num_measures_condition,
+            metadata_condition = metadata_condition_decoder,
+            chord_condition = chord_condition_decoder, 
+            max_gen_len=max_gen_len,
+            temperature=temperature,
+            top_p=top_p,
+            condition_token_lengths = condition_token_lengths, #remove sos token and emotion token
+            chord_token_indices = chord_token_indices,
+            chord_dict_path = chord_dict_path #temporary fix: add chord dict path
+        )
+        results[0]['generation']['content'][0].save(midi_save_path)
 
-                #count how many notes in the right velocity range 
-                correct_vel = count_notes_in_range([x[-1] for x in tokens], find_mapping_vel(metadata_id[-2], additional_token_dict_inv), find_mapping_vel(metadata_id[-1], additional_token_dict_inv)) #TODO: in the future, change this! 
-                total_correct_vel += correct_vel
-                total_vel += len(tokens)
+        #construct metadat diction json
+        metadata_dict = {
+            "key": row["audio_key"],
+            "pitch_range": row["pitch_range"],
+            "num_measures": row["num_measures"],
+            "bpm": row["bpm"],
+            "genre": row["genre"],
+            "track_role": row["track_role"],
+            "inst": row["inst"],
+            "sample_rhythm": row["sample_rhythm"],
+            "time_signature": row["time_signature"],
+            "min_velocity": row["min_velocity"],
+            "max_velocity": row["max_velocity"],
+            "chord_progression": row["chord_progressions"]
+        }
+        with open(metadata_save_path, "w") as f:
+            json.dump(metadata_dict, f)
+        print(f"Generated MIDI saved at: {midi_save_path}, Chord MIDI condition saved at: {chord_save_path}, Metadata saved at: {metadata_save_path}")
 
-                # #count how many notes in the right pitch range 
-                print(f"mean pitch: {np.mean([x[2]*12 + x[3] for x in tokens])}")
-                correct_pitch = count_notes_in_range([np.mean([x[2]*12 + x[3] for x in tokens])], *find_mapping_pitch(metadata_id[1], additional_token_dict_inv))
-                total_correct_pitch += correct_pitch
-                total_pitch += 1
-
-                #the differece (mean and variance) between the chord ending time and the last note of the generated midi 
-                chord_ending_time = chord_tokens[-1][0] + chord_tokens[-1][1]
-                note_ending_time = tokens[-1][0] + tokens[-1][1]
-                difference_chord_music_ending_time.append(chord_ending_time - note_ending_time)
-                #the differece (mean and variance) between the chord ending time and the last note of the generated midi    
-            except Exception as e:
-                print(f"Error: {e}, sub_mid: {sub_mid}")
-                continue
-        #Also save the prompt 
-        print("\n==================================\n")
-
-    print("percentage of correct velocity", total_correct_vel/total_vel)
-    print("percentage of correct pitch", total_correct_pitch/total_pitch)
-    print("mean and variance of the difference between the chord ending time and the last note of the generated midi", np.mean(difference_chord_music_ending_time), np.var(difference_chord_music_ending_time))
 if __name__ == "__main__":
     fire.Fire(main)
