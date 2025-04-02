3d0c1cf887ca4715223d170d5018bce293b746ac
guozixunnicolas <nicolas.guozixun@gmail.com>
2025-04-03 00:11:22 +0100

    change file name from con_gen to uncon_gen

--- recipes/finetuning/real_finetuning_con_gen.py
+++ recipes/finetuning/real_finetuning_con_gen.py
@@ -2,7 +2,7 @@
 # This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
 
 import fire
-from llama_recipes.real_finetuning_con_gen import main
+from llama_recipes.real_finetuning_uncon_gen import main
 
 if __name__ == "__main__":
     fire.Fire(main)
